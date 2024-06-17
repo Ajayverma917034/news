82532b0bff5c6573809d3f23a01e0086d517f028
Ajay Verma <ajayverma917034@gmail.com>
2024-06-17 19:08:24 +0530

    update 3.6

--- server/controller/admin.controller.js
+++ server/controller/admin.controller.js
@@ -1,6 +1,27 @@
-import tryCatch from "../utils/asyncFunction";
+import News from "../model/news.models.js";
+import tryCatch from "../utils/asyncFunction.js";
 
 export const getNews = tryCatch(async (req, res, next) => {
-    const user = req.user;
+    let { page, limit, state, district, location, news_section_type } = req.body;
+    const query = {}
+    if (state) query.state = state;
+    if (district) query.district = district;
+    if (location) query.location = location;
+    if (news_section_type) query.news_section_type = news_section_type;
 
+    limit = parseInt(limit) || 10;
+    page = parseInt(page) || 1;
+
+    News.find({ draft })
+        .skip((page - 1) * limit)
+        .limit(limit)
+        .sort({ createdAt: -1 })
+        .select('news_id title draft createdAt -_id')
+        .then((news) => {
+            return res.status(200).json(news)
+        })
+        .catch(err => {
+            return next(new ErrorHandler(500, err.message))
+        })
 })
+
